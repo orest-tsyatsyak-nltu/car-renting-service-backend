7aa3f544fa0fd2cb3ebaef39aa5219bf21816953
Orest Tsiatsiak <oresttsytsyak02@gmail.com>
2023-10-09 17:41:17 +0300

    Added constraint on phone number now its max length is 15, also added rent entity.

--- src/main/java/com/example/carrentingservicebackend/entity/RentEntity.java
+++ src/main/java/com/example/carrentingservicebackend/entity/RentEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,17 +13,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity(name = "Tenant")
-@Table(
-        name = "tenants",
-        uniqueConstraints = @UniqueConstraint(
-                name = "tenants_phone_number_key",
-                columnNames = "phone_number"
-        )
-)
+@Entity(name = "RentEntity")
+@Table(name = "rents")
 @Getter
 @Setter
 @ToString
@@ -32,32 +28,46 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class TenantEntity {
+public class RentEntity {
     @Id
     @Column(name = "id", nullable = false, columnDefinition="binary(16) not null")
     @GeneratedValue
     private UUID id;
 
     @Column(
-            name = "fullname",
+            name = "car_registration_number",
             nullable = false,
             columnDefinition = "varchar(100)"
     )
-    private String fullName;
+    private String carRegistrationNumber;
 
     @Column(
-            name = "address",
-            nullable = false,
-            columnDefinition = "varchar(100)"
-    )
-    private String address;
-
-    @Column(
-            name = "phone_number",
+            name = "tenant_phone_number",
             nullable = false,
             columnDefinition = "varchar(15)"
     )
-    private String phoneNumber;
+    private String tenantPhoneNumber;
+
+    @Column(
+            name = "rent_date",
+            nullable = false
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime rentDate;
+
+    @Column(
+            name = "return_date",
+            nullable = false
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime returnDate;
+
+    @Column(
+            name = "final_fee",
+            nullable = false,
+            columnDefinition = "decimal"
+    )
+    private BigDecimal finalFee;
 
     @Version
     private Long version;
